b0418f4d860edef36b20a32a3be91e7552f23cd1
Rachel Wyatt <rw126@evansville.edu>
2019-05-09 11:58:26 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  resources :products, only: [:show, :index, :new, :create, :description]
+  resources :products, only: [:show, :index, :new, :create]
+  get 'products/:id/description', to: 'products#description'
 end
